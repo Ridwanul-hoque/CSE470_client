9d67b1d2532fd1cf398dcb23fc6fcf1d701df00c
Ridwanul Hoque <rhr06junhoque@gmail.com>
2025-03-15 23:50:57 +0600

    added the routes

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,6 +4,11 @@ import {
 } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import ContactUs from "../Pages/ContactUs/ContactUs";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import Shop from "../Pages/Shop/Shop";
+import Feature from "../Pages/Feature/Feature";
 
 
 
@@ -16,6 +21,26 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/featured',
+                element: <Feature></Feature>
+            },
+            {
+                path: '/Shop',
+                element: <Shop></Shop>
+            },
+            {
+                path: '/ContactUs',
+                element: <ContactUs></ContactUs>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
             }
         ]
     },
